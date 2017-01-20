b2eb9ef8868cc9d2b55b0c824dc964b2d7beb252
anton <anton.korbalev@gmail.com>
2017-01-21 00:23:09 +0300

    Scaling removed. Minor improvements for correct classification.

--- ClassifierTrainer.py
+++ ClassifierTrainer.py
@@ -9,27 +9,32 @@ class ClassifierTrainer(object):
     def train_classifier(data):
         """ Trains classifier and returns model, best K and accuracy """
         classes = list()
-        classes.append('No info')
+        classes.append('Hold')
 
         for k in range(1, data.index.__len__()):
             if data['<CLOSE>'][k] > data['<CLOSE>'][k - 1]:
-                classes.append('Price grows')
+                classes.append('Buy')
             else:
                 if data['<CLOSE>'][k] < data['<CLOSE>'][k - 1]:
-                    classes.append('Price falls')
+                    classes.append('Sell')
                 else:
-                    classes.append('No info')
+                    classes.append('Hold')
 
         # ignore volume column
-        scaledData = pp.scale(data.take(range(2, 6), axis=1))
+        dataToProcess = data.take(range(2, 6), axis=1)
         kf = ms.KFold(n_splits=5, shuffle=True, random_state=42)
 
         scores = list()
         k_range = range(1, 50)
         for k in k_range:
             model = neigh.KNeighborsClassifier(n_neighbors=k)
-            scs = ms.cross_val_score(model, scaledData, classes, cv=kf, scoring='accuracy')
+            scs = ms.cross_val_score(model, dataToProcess, classes, cv=kf, scoring='accuracy')
             scores.append(scs)
 
         results = pandas.DataFrame(scores, k_range).mean(axis=1).sort_values(ascending=False)
-        return model, results.head(1).index[0], results.head(1).values[0]
+        #train model on best K
+        bestK = results.head(1).index[0]
+        acc = results.head(1).values[0]
+        model = neigh.KNeighborsClassifier(n_neighbors=bestK)
+        model.fit(dataToProcess, classes)
+        return model, bestK, acc
--- KnnProcess.py
+++ KnnProcess.py
@@ -1,9 +1,11 @@
 from StockDataDownloader import StockDataDownloader as d
 from datetime import date
 from ClassifierTrainer import ClassifierTrainer
+import sklearn.preprocessing as pp
 
 downloader = d.StockDataDownloader()
 
-data = downloader.get_data_from_finam('SPFB.SI-9.16', 1, 17, 420658, date(2016,6,13), date(2016,9,12))
+data = downloader.get_data_from_finam('SPFB.SI-6.16', 1, 17, 414253, date(2016,3,11), date(2016,6,12))
 (model, K, acc) = ClassifierTrainer.train_classifier(data)
-print "For SPFB.SI-9.16 best K=",K,'accuracy=',acc
+print 'For SPFB.SI-6.16 best K=',K, 'accuracy=',acc
+
