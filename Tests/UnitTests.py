e8ff979dfd7bcfaa134efcedc1ea533c787c4058
Anton <anton.korbalev@gmail.com>
2017-01-20 16:32:10 +0300

    Changed project structure.

--- Tests/UnitTests.py
+++ Tests/UnitTests.py
@@ -1,3 +1,4 @@
+from __future__ import absolute_import
 from StockDataDownloader import StockDataDownloader
 from datetime import date
 import unittest
@@ -7,7 +8,7 @@ class DownloaderTests(unittest.TestCase):
     """Test methods for StockDataDownloader"""
 
     def test_downloader(self):
-        downloader = StockDataDownloader()
+        downloader = StockDataDownloader.StockDataDownloader()
         data = downloader.get_data_from_finam('SPFB.SI-9.16', 5, 17, 420658, date(2016,6,13), date(2016,9,12))
         self.assertTrue(data.__len__() == 910, 'Invalid number of rows!')
         self.assertTrue(data.shape[1] == 7, 'Invalid number of columns!')
