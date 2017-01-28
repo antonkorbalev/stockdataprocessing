cb6f780959be85c0c79f5e06f543cde1f738558f
anton <anton.korbalev@gmail.com>
2017-01-28 13:20:41 +0300

    Configuration file.

---
+++ Config.py
@@ -0,0 +1,7 @@
+
+class Config:
+    def __init__(self):
+        self.token = open('Token.txt', 'r').read()
+        self.insName = 'EUR_USD'
+        self.maxLength = 100  # ticks
+        self.period = 1  # s
--- FxRobot.py
+++ FxRobot.py
@@ -1,25 +1,21 @@
 import oandapy
 import matplotlib.pyplot as plt
+from Config import Config
 
-
-token = open('Token.txt','r').read()
-insName = 'EUR_USD'
-maxLength=100 # ticks
-period = 1 #s
-
-oanda = oandapy.API(environment="practice", access_token=token)
+config = Config()
+oanda = oandapy.API(environment="practice", access_token = config.token)
 asks = list()
 bids = list()
 price_change = list()
 
 def get_prices():
-    response = oanda.get_prices(instruments=insName)
+    response = oanda.get_prices(instruments=config.insName)
     prices = response.get('prices')
     ask = prices[0].get('ask')
     bid = prices[0].get('bid')
     status = prices[0].get('status')
     if status == 'halted':
-        print insName, 'is halted.'
+        print config.insName, 'is halted.'
         return
     asks.append(ask)
     bids.append(bid)
@@ -27,17 +23,14 @@ def get_prices():
     plt.clf()
     price_change.append((ask+bid)/2 - lastPrice)
     plt.plot(price_change, label='Price change', color='red')
-    #plt.plot(asks, label='ASK', color='red')
-    #plt.plot(bids, label='BID', color='green')
-    plt.title(insName)
+    plt.title(config.insName)
     plt.xlabel('Time, s')
-    #plt.ylabel('Price '+ insName)
     plt.legend(loc='upper right')
-    if (len(asks)>maxLength):
+    if (len(asks)>config.maxLength):
         asks.pop(0)
-    if (len(bids) > maxLength):
+    if (len(bids) > config.maxLength):
         bids.pop(0)
-    if (len(price_change) > maxLength):
+    if (len(price_change) > config.maxLength):
         price_change.pop(0)
 
 plt.ion()
@@ -45,6 +38,6 @@ plt.grid(True)
 
 while True:
     get_prices()
-    plt.pause(period)
+    plt.pause(config.period)
 
 
