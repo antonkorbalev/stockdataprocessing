70dbcfaf884f37a19c9ec4300e4494e09b76a609
anton <anton.korbalev@gmail.com>
2017-01-28 13:50:54 +0300

    Saving prices logs for back testing.

--- Config.py
+++ Config.py
@@ -1,7 +1,8 @@
-
 class Config:
     def __init__(self):
         self.token = open('Token.txt', 'r').read()
         self.insName = 'EUR_USD'
         self.maxLength = 100  # ticks
         self.period = 1  # s
+        self.write_back_log = True
+        self.back_log_path = 'BackLog'
--- FxRobot.py
+++ FxRobot.py
@@ -1,12 +1,18 @@
 import oandapy
 import matplotlib.pyplot as plt
 from Config import Config
+from os import path
+import datetime
 
 config = Config()
 oanda = oandapy.API(environment="practice", access_token = config.token)
 asks = list()
 bids = list()
 price_change = list()
+f_back_log = open(path.relpath(config.back_log_path + '/' + config.insName + '_' + datetime.datetime.now().strftime("%Y%m%d-%H%M%S")), 'a');
+if config.write_back_log:
+    print 'Backlog file name:', f_back_log.name
+    f_back_log.write('DateTime,Instrument,ASK,BID,Status \n')
 
 def get_prices():
     response = oanda.get_prices(instruments=config.insName)
@@ -14,6 +20,7 @@ def get_prices():
     ask = prices[0].get('ask')
     bid = prices[0].get('bid')
     status = prices[0].get('status')
+    f_back_log.write('%s,%s,%s,%s,%s \n' % (datetime.datetime.now(), config.insName, prices[0].get('ask'), prices[0].get('bid'), prices[0].get('status')))
     if status == 'halted':
         print config.insName, 'is halted.'
         return
