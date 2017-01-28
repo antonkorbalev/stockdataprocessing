976aa3acc1be1f4f9ad09ce7c4e3c7dc1ab5d380
anton <anton.korbalev@gmail.com>
2017-01-28 12:24:48 +0300

    Do not consider repeated prices.

--- FxRobot.py
+++ FxRobot.py
@@ -1,7 +1,5 @@
 import oandapy
-import multiprocessing
 import matplotlib.pyplot as plt
-import numpy as np
 
 
 token = open('Token.txt','r').read()
@@ -15,23 +13,34 @@ bids = list()
 def get_prices():
     response = oanda.get_prices(instruments=insName)
     prices = response.get('prices')
-    asks.append(prices[0].get('ask'))
-    bids.append(prices[0].get('bid'))
+    ask = prices[0].get('ask')
+    bid = prices[0].get('bid')
+    if len(asks) > 0 and len(bids) > 0:
+        if asks[len(asks)-1]!=ask and bids[len(bids)-1]!=bid:
+            asks.append(ask)
+            bids.append(bid)
+        else:
+            print prices
+    else:
+        asks.append(ask)
+        bids.append(bid)
     plt.clf()
-    plt.plot(asks, label='ASK')
-    plt.plot(bids, label='BID')
+    plt.plot(asks, label='ASK', color='red')
+    plt.plot(bids, label='BID', color='green')
+    plt.title(insName)
+    plt.xlabel('Time, s')
+    #plt.ylabel('Price '+ insName)
     plt.legend(loc='upper right')
     if (len(asks)>maxLength):
         asks.pop(0)
     if (len(bids) > maxLength):
         bids.pop(0)
 
-
 plt.ion()
-
+plt.grid(True)
 
 while True:
     get_prices()
-    plt.pause(1)
+    plt.pause(0.05)
 
 
