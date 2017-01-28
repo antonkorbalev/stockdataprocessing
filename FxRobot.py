d20f61b1f5c8bb7a141df53a33f11a6c169493be
anton <anton.korbalev@gmail.com>
2017-01-28 12:41:31 +0300

    Market status. Plot and save price change.

--- FxRobot.py
+++ FxRobot.py
@@ -4,29 +4,31 @@ import matplotlib.pyplot as plt
 
 token = open('Token.txt','r').read()
 insName = 'EUR_USD'
-maxLength=100
+maxLength=100 # ticks
+period = 1 #s
 
 oanda = oandapy.API(environment="practice", access_token=token)
 asks = list()
 bids = list()
+price_change = list()
 
 def get_prices():
     response = oanda.get_prices(instruments=insName)
     prices = response.get('prices')
     ask = prices[0].get('ask')
     bid = prices[0].get('bid')
-    if len(asks) > 0 and len(bids) > 0:
-        if asks[len(asks)-1]!=ask and bids[len(bids)-1]!=bid:
-            asks.append(ask)
-            bids.append(bid)
-        else:
-            print prices
-    else:
-        asks.append(ask)
-        bids.append(bid)
+    status = prices[0].get('status')
+    if status == 'halted':
+        print insName, 'is halted.'
+        return
+    asks.append(ask)
+    bids.append(bid)
+    lastPrice = (asks[len(asks)-1] + bids[len(bids)-1]) / 2
     plt.clf()
-    plt.plot(asks, label='ASK', color='red')
-    plt.plot(bids, label='BID', color='green')
+    price_change.append((ask+bid)/2 - lastPrice)
+    plt.plot(price_change, label='Price change', color='red')
+    #plt.plot(asks, label='ASK', color='red')
+    #plt.plot(bids, label='BID', color='green')
     plt.title(insName)
     plt.xlabel('Time, s')
     #plt.ylabel('Price '+ insName)
@@ -35,12 +37,14 @@ def get_prices():
         asks.pop(0)
     if (len(bids) > maxLength):
         bids.pop(0)
+    if (len(price_change) > maxLength):
+        price_change.pop(0)
 
 plt.ion()
 plt.grid(True)
 
 while True:
     get_prices()
-    plt.pause(0.05)
+    plt.pause(period)
 
 
