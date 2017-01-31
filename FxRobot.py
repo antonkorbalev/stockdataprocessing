af5a885a2095484ab419a0d539a758fa18c441c6
anton <anton.korbalev@gmail.com>
2017-01-31 21:02:22 +0300

    Min max calculation.

--- FxRobot.py
+++ FxRobot.py
@@ -3,6 +3,7 @@ import matplotlib.pyplot as plt
 from Config import Config
 from os import path
 import datetime
+import numpy
 
 config = Config()
 oanda = oandapy.API(environment="practice", access_token = config.token)
@@ -12,12 +13,13 @@ price_change = list()
 f_back_log = open(path.relpath(config.back_log_path + '/' + config.insName + '_' + datetime.datetime.now().strftime("%Y%m%d-%H%M%S"))+'.log', 'a');
 time = 0
 times = list()
+last_result = 'hold'
 
 if config.write_back_log:
     print 'Backlog file name:', f_back_log.name
     f_back_log.write('DateTime,Instrument,ASK,BID,Price change,Status \n')
 
-def get_prices():
+def get_real_prices():
     response = oanda.get_prices(instruments=config.insName)
     prices = response.get('prices')
     ask = prices[0].get('ask')
@@ -35,27 +37,67 @@ def get_prices():
     price_change.append(pChange)
     if config.write_back_log:
         f_back_log.write('%s,%s,%s,%s,%s,%s \n' % (datetime.datetime.now(), config.insName, prices[0].get('ask'), prices[0].get('bid'), pChange, prices[0].get('status')))
+    result = process_data(price_change)
+    global last_result
+    if result != 'hold':
+        last_result = result
+    print time, "s : ", result, ' spread size: ', ask - bid
+
+def process_data(price_change):
+    result = 'hold'
+    global last_result
     plt.clf()
-    times.append(time);
-    plt.plot(times, price_change, label='Price change', color='blue')
+    times.append(time)
+    hmin = 0
+    hmax = 0
+    if len(price_change) > 3:
+        hmaxs = list()
+        for i in range(1, len(price_change) - 2):
+            if price_change[i] > price_change[i-1] and price_change[i] > price_change[i+1]:
+                hmaxs.append(price_change[i])
+        if len(hmaxs) > 0:
+            hmax = numpy.mean(hmaxs)
+            plt.plot([times[0],times[len(times)-1]], [hmax, hmax], label='MAX', color='red', linestyle=':')
+        hmins = list()
+        for i in range(1, len(price_change) - 2):
+            if price_change[i] < price_change[i-1] and price_change[i] < price_change[i+1]:
+                hmins.append(price_change[i])
+        if len(hmins) > 0:
+            hmin = numpy.mean(hmins)
+            plt.plot([times[0],times[len(times)-1]], [hmin, hmin], label='MIN', color='green', linestyle=':')
+    if hmin != 0 and hmax != 0:
+        if price_change[len(price_change)-1] >= hmax:
+            if last_result != 'sell':
+                result = 'sell'
+        if price_change[len(price_change)-1] <= hmin:
+            if last_result != 'buy':
+                result = 'buy'
+        if price_change[len(price_change) - 1] > hmin and price_change[len(price_change)-1] < hmax:
+            if last_result != 'close' and last_result!='hold':
+                result = 'close'
+
+    plt.plot(times, price_change, label='Price change', color='blue',  marker='')
     plt.title(config.insName)
     plt.xlabel('Time, s')
 
-    plt.legend(loc='upper right')
-    if (len(asks)>config.maxLength):
+    plt.legend(loc='upper left')
+    if len(asks) > config.maxLength:
         asks.pop(0)
-    if (len(bids) > config.maxLength):
+    if len(bids) > config.maxLength:
         bids.pop(0)
-    if (len(price_change) > config.maxLength):
+    if len(price_change) > config.maxLength:
         price_change.pop(0)
-    if (len(times) > config.maxLength):
+    if len(times) > config.maxLength:
         times.pop(0)
+
+    return result
+
 
 plt.ion()
 plt.grid(True)
 
 while True:
-    get_prices()
+    get_real_prices()
     plt.pause(config.period)
     time = time + config.period
 
