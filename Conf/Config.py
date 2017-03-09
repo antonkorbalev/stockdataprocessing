4912ce4fbd7f471773c3f8e3c4aa40c45e2dd4cc
Anton <anton.korbalev@gmail.com>
2017-03-09 13:41:46 +0300

    Config fixes.

--- Conf/Config.py
+++ Conf/Config.py
@@ -1,13 +1,13 @@
 class Config:
     def __init__(self):
-        self.token = open('../Account/Token.txt', 'r').read()
+        self.token = open('Account/Token.txt', 'r').read()
         self.env = 'practice'
         self.insName = 'EUR_USD'
         self.maxLength = 20 # ticks
         self.period = 1 # s
         self.write_back_log = False
         self.back_log_path = 'BackLog'
-        self.account_id = open('../Account/Account.txt', 'r').read()
+        self.account_id = open('Account/Account.txt', 'r').read()
         self.stop_loss_value = 0
         self.take_profit_value = 0
         self.lot_size = 10000
